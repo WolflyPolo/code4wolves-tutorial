9497d944208b8d60e928c5c2cd04397ecd85330e
Samuel Polo <samuelpp1996@hotmail.com>
2018-08-19 20:39:16 -0500

    Videos CRUD Updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_18_173845) do
+ActiveRecord::Schema.define(version: 2018_08_20_013210) do
 
   create_table "videos", force: :cascade do |t|
     t.string "wistia"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
